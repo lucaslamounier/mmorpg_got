68a242d5cef97ced695a02ae9da5fe08989a3032
Lucas Lamounier <lucaslamounier@users.noreply.github.com>
2017-08-08 17:06:22 -0300

    finalizando o jogo

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,21 +1,89 @@
 module.exports.jogo = function(application, req, res){
+      // Verifica se o usuário está autenticado
       if(req.session.autorizado !== true){
           res.send('Usuário precisa fazer login');
           return;
       };
 
+      var msg = '';
+
+      if(req.query.msg !== ''){
+          msg = req.query.msg;
+      };
       var usuario = req.session.usuario;
       var casa = req.session.casa;
       // pega a variavel de conexão com o mongoDB
       var connection = application.config.dbConnection;
       var JogoDAO  = new application.app.models.JogoDAO(connection);
 
-      JogoDAO.iniciaJogo(res, usuario, casa);
-      //res.render('jogo', {img_casa: req.session.casa});
+      JogoDAO.iniciaJogo(res, usuario, casa, msg);
 };
 
 module.exports.sair = function(application, req, res){
       req.session.destroy(function(err, result){
             res.render('index', {validacao: {}, dadosForm: {} });
       });
+};
+
+module.exports.suditos = function(application, req, res){
+      // Verifica se o usuário está autenticado
+      if(req.session.autorizado !== true){
+          res.send('Usuário precisa fazer login');
+          return;
+      };
+      res.render("aldeoes");
+};
+
+module.exports.pergaminhos = function(application, req, res){
+    // Verifica se o usuário está autenticado
+    if(req.session.autorizado !== true){
+        res.send('Usuário precisa fazer login');
+        return;
+    };
+    /* Recuperar as ações inseridas no banco de dados */
+    var connection = application.config.dbConnection;
+    var JogoDAO  = new application.app.models.JogoDAO(connection);
+    var usuario = req.session.usuario;
+
+    JogoDAO.getAcoes(usuario, res);
+};
+
+
+module.exports.ordenar_acao_sudito = function(application, req, res){
+    // Verifica se o usuário está autenticado
+    if(req.session.autorizado !== true){
+        res.send('Usuário precisa fazer login');
+        return;
+    };
+
+    var dadosForm = req.body;
+
+    req.assert('acao', 'Ação deve ser informada').notEmpty();
+    req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.redirect('jogo?msg=A');
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var JogoDAO  = new application.app.models.JogoDAO(connection);
+
+    dadosForm.usuario = req.session.usuario;
+    JogoDAO.acao(dadosForm);
+
+    res.redirect('jogo?msg=B');
+};
+
+
+module.exports.revogar_acao = function(application, req, res){
+    var url_query = req.query;
+    var _id = url_query.id_acao;
+    var connection = application.config.dbConnection;
+    var JogoDAO  = new application.app.models.JogoDAO(connection);
+
+    JogoDAO.revogarAcao(_id, res)
+    
 };
--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -1,3 +1,6 @@
+// ObjectID para remoção dos documentos dentro do mongodb
+var ObjectID = require('mongodb').ObjectID;
+
 function JogoDAO(connection){
     this._connection = connection();
 };
@@ -24,21 +27,99 @@ JogoDAO.prototype.gerarParametros = function(usuario){
 
 };
 
-JogoDAO.prototype.iniciaJogo = function(res, usuario, casa){
+JogoDAO.prototype.iniciaJogo = function(res, usuario, casa, msg){
   // Abre a conexão com o banco de dados
   this._connection.open(function(err, mongoclient){
         mongoclient.collection("jogo", function(err, collection){
             // consulta os usuário no banco de dados
             collection.find({usuario: usuario}).toArray(function(err, result){
-                console.log(result[0]);
+
                 res.render("jogo",
-                    { img_casa: casa, jogo: result[0] }
+                    { img_casa: casa, jogo: result[0], msg: msg }
                 );
                 // Encerra conexão com o banco de dados
                 mongoclient.close();
             }); // end find
       }); // end collection
   }); // end open
+};
+
+JogoDAO.prototype.acao = function(acao, usuario){
+
+  // Abre a conexão com o banco de dados
+  this._connection.open(function(err, mongoclient){
+      mongoclient.collection("acao", function(err, collection){
+          var date = new Date();
+          var tempo = null;
+
+          switch (parseInt(acao.acao)) {
+            case 1: tempo = 1 * 60 * 60000; break;
+            case 2: tempo = 2 * 60 * 60000; break;
+            case 3: tempo = 5 * 60 * 60000; break;
+            case 4: tempo = 5 * 60 * 60000; break;
+          };
+
+          acao.acao_termina_em = date.getTime() + tempo; // 01/01/1970 até o instante que a função foi executada
+          // Insere os dados no banco de dados
+          collection.insert(acao);
+      });
+      mongoclient.collection("jogo", function(err, collection){
+
+          var moedas = null;
+
+          switch (parseInt(acao.acao)) {
+            case 1: moedas = -2 * acao.quantidade; break;
+            case 2: moedas = -3 * acao.quantidade; break;
+            case 3: moedas = -1 * acao.quantidade; break;
+            case 4: moedas = -1 * acao.quantidade; break;
+          };
+
+          collection.update(
+            { usuario: acao.usuario},
+            { $inc: {moeda: moedas} }
+          );
+          // Encerra conexão com o banco de dados
+          mongoclient.close();
+      });
+
+  });
+
+};
+
+JogoDAO.prototype.getAcoes = function(usuario, res){
+      // Abre a conexão com o banco de dados
+      this._connection.open(function(err, mongoclient){
+            mongoclient.collection("acao", function(err, collection){
+                    var date = new Date();
+                    var momento_atual = date.getTime();
+                    // consulta os usuário no banco de dados
+                    collection.find({
+                        usuario: usuario,
+                        acao_termina_em: {$gt: momento_atual}
+                    }).toArray(function(err, result){
+                        res.render("pergaminhos", { acoes: result });
+                        // Encerra conexão com o banco de dados
+                        mongoclient.close();
+                    }); // end find
+          }); // end collection
+      }); // end open
+};
+
+JogoDAO.prototype.revogarAcao = function(_id, res){
+
+    // Abre a conexão com o banco de dados
+    this._connection.open(function(err, mongoclient){
+          mongoclient.collection("acao", function(err, collection){
+                // Removendo documentos
+                collection.remove(
+                    {_id : ObjectID(_id) },
+                    function(err, result){
+                        res.redirect("jogo?msg=D");
+                        mongoclient.close();
+                    }
+                );
+        }); // end collection
+    }); // end open
 };
 
 module.exports = function(){
